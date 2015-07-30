5d5c6e7666d572e0f164e0bf04adbe05a3c4fdd5
systemAce24185 <krishoza@krishs-mbp.elfridaps.internal>
2015-07-30 14:30:07 +0100

    We did It!!!

--- myday.php
+++ myday.php
@@ -1,4 +1,31 @@
 <?php
+function getFoodRecs($location){
+  $ch = curl_init();
+    
+  curl_setopt($ch, CURLOPT_URL, "http://api.ratings.food.gov.uk/Establishments?longitude=0&latitude=51.4&pageNumber=1&pageSize=10&maxDistanceLimit=1&sortOptionKey=Rating_desc");
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('x-api-version:2', 'accept: text/json'));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $json = curl_exec($ch);
+  curl_close($ch);      
+  
+
+  $data = json_decode($json);
+    
+  $listData = array();
+  foreach ($data->establishments as $item) {
+    $listData[] = array(
+      'name' => $item->BusinessName,
+      'address' => $item->AddressLine1,
+      'type' => $item->BusinessType,
+      'rating' => $item->RatingValue,
+    );
+  }
+  
+  return $listData;
+}
+
+
+
 $mood = $_POST["mood"];
 $travel = $_POST["Method_of_Travel"];
 $budget = $_POST["Budget"];
@@ -11,7 +38,10 @@ $location = array(
 switch ($mood) {
     case "hungry":
         echo "You are hungry. :P";
+        $results = getFoodRecs($location);
         break;
     default:
-        echo "You are " . $mood . ", But we don't support it yet sorry ;(";
+      	echo "You are " . $mood . ", But we don't support it yet sorry ;(";
 }
+
+
