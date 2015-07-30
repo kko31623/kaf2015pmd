2c5f905325cd754a0c30485e9a4c05eb2adebc79
Krish <systemAce24185@users.noreply.github.com>
2015-07-30 11:12:35 +0100

    Update myday.php

--- myday.php
+++ myday.php
@@ -6,4 +6,12 @@ $time = $_POST["Time"];
 $location = array(
   "lat" => 50,
   "lng" => 0
-  );
+);
+
+switch ($mood) {
+    case "Hungry":
+        echo "You are " . $mood;
+        break;
+    case default:
+      echo "You are" . $mood . ", But we don't support it yet sorry ;(";
+}
