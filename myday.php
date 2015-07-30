7a7088d694fbf962199b3d3831e8bfe9eb93c68f
Krish <systemAce24185@users.noreply.github.com>
2015-07-30 10:56:59 +0100

    Update myday.php

--- myday.php
+++ myday.php
@@ -1,2 +1,9 @@
 <?php
-var_dump($_POST);
+$mood = $_POST["mood"];
+$travel = $_POST["Method_of_Travel"];
+$budget = $_POST["Budget"];
+$time = $_POST["Time"];
+$location = array(
+  "lat" => 50,
+  "lng" => 0
+  );
